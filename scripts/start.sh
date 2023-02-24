740e0aa54c56ca5d6f963dd496ce4d52fe87831f
Carter Jones <carter@carterjones.info>
2023-02-24 09:52:46 -0800

    fix execution of custom start command

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -ex -o pipefail
 
-sh -c "${START_COMMAND:-forever start --id 'vulnogram' --spinSleepTime 5000 --minUptime 2000 app.js; forever list}"
+if [[ -z "${START_COMMAND}" ]]; then
+    forever start --id 'vulnogram' --spinSleepTime 5000 --minUptime 2000 app.js
+    forever list
+else
+    /bin/bash -c "${START_COMMAND}"
+fi
