32c211f26ac52d2d7bb3b839f449950d8017a97e
Chandan <chandanbn@gmail.com>
2025-11-01 20:09:17 -0700

    Refactor SSVC to match CISA schema

--- default/cve5/preload.js
+++ default/cve5/preload.js
@@ -113,3 +113,89 @@ JSONEditor.defaults.resolvers.unshift(function (schema) {
         return "CPEA";
     }
 });
+
+class ssvcOpts extends JSONEditor.defaults.editors.array {
+    setValue(value) {
+        this.value = value;
+        if (value && value.length > 0) {
+            value.forEach(sm => {
+                var m = Object.keys(sm)[0];
+                if (m && this.editorMap[m]) {
+                    this.editorMap[m].setValue(sm[m]);
+                }
+            });
+        }
+        console.log(value);
+    }
+
+    getValue() {
+        console.log('called getValue');
+        var ret = [];
+        if (this.editorMap) {
+            for (const key in this.editorMap) {
+                if (this.editorMap.hasOwnProperty(key)) {
+                    const value = this.editorMap[key].getValue();
+                    ret.push({ [key]: value });
+                }
+            }
+        }
+        return ret;
+    }
+
+    build() {
+        super.build();
+        console.log(this.schema);
+        // Clear any existing editor containers to rebuild subeditors
+        if (this.editors && this.editors.length) {
+            this.editors.forEach(editor => editor.destroy());
+            this.editorMap = {};
+            this.editors = [];
+            if (this.container) {
+                this.container.innerHTML = '';
+            }
+        } else {
+            this.editors = [];
+            this.editorMap = {};
+        }
+
+        // Create subeditors for each object in this.value (ordered array)
+        Object.keys(this.schema.items.properties).forEach(metric => {
+            var row = document.createElement('div');
+            row.className = 'row';
+            var itemName = metric;//Object.keys(metric.properties)[0];
+            var itemSchema = this.schema.items.properties[itemName];
+            var itemLabel = document.createElement('label');
+            itemLabel.className = 'lbl';
+            itemLabel.innerText = itemName;
+            row.appendChild(itemLabel);
+            var editor = this.jsoneditor.createEditor(JSONEditor.defaults.editors.radio, {
+                jsoneditor: this.jsoneditor,
+                schema: itemSchema,
+                path: `${this.path}.${itemName}`,
+                parent: this,
+                compact: true,
+                required: true
+            });
+            editor.container = row;
+            editor.preBuild();
+            editor.build();
+            editor.postBuild();
+            console.log(editor);
+            // Append editor container to this container
+            if (this.container) {
+                this.container.appendChild(row);
+            }
+
+            this.editors.push(editor);
+            this.editorMap[itemName] = editor;
+        });
+    }
+}
+
+JSONEditor.defaults.editors.ssvcOpts = ssvcOpts;
+
+JSONEditor.defaults.resolvers.unshift(function (schema) {
+    if (schema.format === 'ssvcOpts') {
+        return 'ssvcOpts';
+    }
+});
