37c2eeab6e41c45b975b38fd8b0456ad39309d6c
Chandan <chandanbn@gmail.com>
2025-11-19 16:40:42 -0800

    Trigger history saves on the whole doc change, not while loading a vector.

--- default/cvss4/postload.js
+++ default/cvss4/postload.js
@@ -1,4 +1,9 @@
 docEditor.on('ready', async () => {
-   // console.log('doc Ready!');
+   console.log('doc Ready!');
 });
 
+docEditor.on('change', () => {
+      console.log('doc editor changed');
+      var v = docEditor.getValue();
+      onCalcChange(v.vectorString, v.baseScore);
+});
