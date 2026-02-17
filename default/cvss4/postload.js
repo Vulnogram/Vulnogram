66a329605c25a9e580fd10209c0a2861cb6a6f90
Chandan <chandanbn@gmail.com>
2026-02-17 14:07:37 -0800

    remove unused code

--- default/cvss4/postload.js
+++ default/cvss4/postload.js
@@ -1,9 +1,5 @@
-docEditor.on('ready', async () => {
-   console.log('doc Ready!');
-});
 
 docEditor.on('change', () => {
-      console.log('doc editor changed');
       var v = docEditor.getValue();
       onCalcChange(v.vectorString, v.baseScore);
 });
